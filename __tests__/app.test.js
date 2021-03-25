834e4417b717607008c8ad61649e0a31ef40522d
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-03-25 09:32:32 -0700

    delete test passes in easiest way possible.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,56 +39,62 @@ describe('03_separation-of-concerns-demo routes', () => {
   });
 
   it('returns all orders in the database', async () => {
-    await request(app)
-    .post('/api/v1/orders')
-    .send({ quantity: 10 });
-    await request(app)
-    .post('/api/v1/orders')
-    .send({ quantity: 1000 });
-    
-    const orders = await request(app)
-      .get('/api/v1/orders')
-  
-    expect(orders.body).toEqual([{
-      id: '1',
-      quantity: 10,
-    },
-  {
-      id: '2',
-      quantity: 1000,
-  }])
-  
+    await request(app).post('/api/v1/orders').send({ quantity: 10 });
+    await request(app).post('/api/v1/orders').send({ quantity: 1000 });
+
+    const orders = await request(app).get('/api/v1/orders');
+
+    expect(orders.body).toEqual([
+      {
+        id: '1',
+        quantity: 10,
+      },
+      {
+        id: '2',
+        quantity: 1000,
+      },
+    ]);
   });
 
   it('it returns one specific order by its ID', async () => {
     const order = await request(app)
-    .post('/api/v1/orders')
-    .send({ quantity: 10 });
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
 
-    const result = await request(app)
-      .get(`/api/v1/orders/${order.body.id}`)
-    
-    expect(result.body).toEqual([{
-      id: '1',
-      quantity: 10,
-    }])
+    const result = await request(app).get(`/api/v1/orders/${order.body.id}`);
 
-
-  })
+    expect(result.body).toEqual([
+      {
+        id: '1',
+        quantity: 10,
+      },
+    ]);
+  });
 
   it('it should alter an existing order by ID', async () => {
     const order = await request(app)
-    .post('/api/v1/orders')
-    .send({ quantity: 10 });
-console.log(order.body.id);
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+
     const whatItBeNow = await request(app)
-    .put(`/api/v1/orders/${order.body.id}`)
-    .send({ quantity: 500 });
-console.log(whatItBeNow.body, 'tests')
+      .put(`/api/v1/orders/${order.body.id}`)
+      .send({ quantity: 500 });
+
     expect(whatItBeNow.body).toEqual({
       id: '1',
       quantity: 500,
-    })
-  })
-});
+    });
+  });
 
+  it('It should delete an order quantity by its id', async () => {
+    const order = await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+    const wrongOrder = await request(app)
+      .delete(`/api/v1/orders/${order.body.id}`)
+      
+    
+
+    expect(wrongOrder.body).toEqual({});
+  });
+});
